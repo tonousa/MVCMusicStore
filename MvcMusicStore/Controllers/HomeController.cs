bfce9fbe690e04d1978a756ccc1c865975b061f1
tonousa <tonousa@hotmail.com>
2017-10-13 19:49:17 -0400

    helpers

--- MvcMusicStore/Controllers/HomeController.cs
+++ MvcMusicStore/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcMusicStore.Models;
 
 namespace MvcMusicStore.Controllers
 {
     public class HomeController : Controller
     {
+        private MusicStoreDB storeDB = new MusicStoreDB();
+
         public ActionResult Index()
         {
             return View();
@@ -30,6 +33,16 @@ namespace MvcMusicStore.Controllers
         public ActionResult Edit()
         {
             return View();
+        }
+
+        public ActionResult Search(string q)
+        {
+            var albums = storeDB.Albums
+                .Include("Artist")
+                .Where(a => a.Title.Contains(q))
+                .Take(10);
+
+            return View(albums);
         }
     }
 }
--- MvcMusicStore/Global.asax.cs
+++ MvcMusicStore/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Data.Entity;
+using MvcMusicStore.Models;
 
 namespace MvcMusicStore
 {
@@ -12,6 +14,8 @@ namespace MvcMusicStore
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(new MusicStoreDBInitializer());
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
